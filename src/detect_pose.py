2f751d6c9fd0da4dca806d4e0ee24c746076ffcc
Roberto Canale <roberto.canale.work@gmail.com>
2022-08-19 12:02:28 +0800

    fixed the download and got it working better

---
+++ src/detect_pose.py
@@ -0,0 +1,9 @@
+
+import matplotlib.pyplot as plt
+import torch
+import cv2
+from torchvision import transforms
+import numpy as np
+from utils.datasets import letterbox
+from utils.general import non_max_suppression_kpt
+from utils.plots import output_to_keypoint, plot_skeleton_kpts
--- src/utils/google_utils.py
+++ src/utils/google_utils.py
@@ -18,7 +18,7 @@ def gsutil_getsize(url=''):
 
 def attempt_download(file, repo='WongKinYiu/yolov7'):
     # Attempt file download if does not exist
-    file = Path(str(file).strip().replace("'", '').lower())
+    file = Path(str(file).strip().replace("'", ''))
 
     if not file.exists():
         try:
@@ -31,7 +31,7 @@ def attempt_download(file, repo='WongKinYiu/yolov7'):
                     if key == "tag_name":
                         tag = x['tag_name']  # i.e. 'v1.0'
         except:  # fallback plan
-            assets = ['yolov7.pt']
+            assets = ['yolov7.pt', 'yolov7-tiny.pt', 'yolov7x.pt', 'yolov7-d6.pt', 'yolov7-e6.pt', 'yolov7-e6e.pt', 'yolov7-w6.pt']            
             tag = subprocess.check_output('git tag', shell=True).decode().split()[-1]
 
         name = file.name
